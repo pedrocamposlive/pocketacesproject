4bb99de8a138b958007963a8052198e08a107e89
pedrocamposlive <145505523+pedrocamposlive@users.noreply.github.com>
2025-04-03 18:39:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Poker app no ar!"
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
